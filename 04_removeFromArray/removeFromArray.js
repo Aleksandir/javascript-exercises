b5caecfa4257f08f602281686cc3dcf0b7bbde22
Aleks B <aleksandir@me.com>
2025-03-16 21:49:11 +1030

    feat: implement removeFromArray function to remove specified values from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function () {};
+const removeFromArray = function (list, ...rest) {
+  let removingValue = rest;
+  index = 0;
+  for (let x = removingValue.length; x > 0; x--) {
+    index = list.indexOf(removingValue[x - 1]);
+    while (index > -1) {
+      list.splice(index, 1);
+      index = list.indexOf(removingValue[x - 1]);
+    }
+  }
+  return list;
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
