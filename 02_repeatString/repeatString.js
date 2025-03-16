e20d85c2c78520652689f4af7b50a431ca6fe865
Aleks B <aleksandir@me.com>
2025-03-16 20:37:38 +1030

    feat: implement repeatString function to repeat a string a specified number of times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function () {};
+const repeatString = function (string, num) {
+  let repeatedString = "";
+  if (num > -1) {
+    for (let i = 1; i <= num; i++) {
+      repeatedString = repeatedString + string;
+    }
+    return repeatedString;
+  } else return "ERROR";
+};
 
 // Do not edit below this line
 module.exports = repeatString;
