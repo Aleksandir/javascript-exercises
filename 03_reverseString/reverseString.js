7e5b341109053bed199e243375230a7a2b333c63
Aleks B <aleksandir@me.com>
2025-03-16 20:54:45 +1030

    feat: implement reverseString function to reverse a given string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function () {};
+const reverseString = function (string) {
+  let reversedString = "";
+  for (let charCount = string.length; charCount > -1; charCount--) {
+    reversedString += string.charAt(charCount);
+  }
+  return reversedString;
+};
+
+//Could have solved usinf "return string.split("").reverse().join("");""
 
 // Do not edit below this line
 module.exports = reverseString;
