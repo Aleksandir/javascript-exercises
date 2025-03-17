094130ddb5507b3dc39b4f48181422f48aaa1511
Aleks B <aleksandir@me.com>
2025-03-18 10:26:42 +1030

    feat: implement leapYears function to determine if a year is a leap year
    fix: add error handling for non-integer input

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function () {};
+const leapYears = function (year) {
+  if (!Number.isInteger(year)) {
+    return "ERROR";
+    //to make this cleaner you could declare constants above and name the variables what you are checking for such as "isCentury = year % 100 === 0;"
+  } else if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Do not edit below this line
 module.exports = leapYears;
